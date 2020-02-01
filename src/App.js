9ad80417e30487d0f03e8d173a74a775720131cf
Maggie C <maggiec@Charlies-MacBook-Pro.local>
2020-02-01 18:57:21 -0500

    landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Login from "./login";
-import Register from "./register";
+import LandingPage from "./components/landingpage";
+import Login from "./components/login";
+import Register from "./components/register"
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 class App extends React.Component {
