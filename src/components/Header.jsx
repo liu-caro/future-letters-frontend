d385562b3cb16309d0e512b1d35efe0426036b0c
Maggie C <maggiec@Charlies-MacBook-Pro.local>
2020-02-02 02:37:47 -0500

    logo woo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,6 +16,7 @@
 
 */
 import React from "react";
+import LogoBottle from "../images/LogoBottle.png";
 
 // reactstrap components
 import { Button, Card, CardBody, CardTitle, Container, Row, Col } from "reactstrap";
@@ -24,7 +25,7 @@ class Header extends React.Component {
   render() {
     return (
       <>
-        <div className="header bg-gradient-info pb-8 pt-5 pt-md-8">
+        <div className="header bg-gradient-info pb-8 pt-5 pt-md-0">
           <Container fluid>
             <div className="header-body">
               {/* Card stats */}
